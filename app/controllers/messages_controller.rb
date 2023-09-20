afb918eb09309e72cdc1766d43faa27a40dbcf6e
Umut Akturk <umut.akturk14@gmail.com>
2023-09-20 19:13:49 +0300

    Working on the view page for messaging. Going to add AJAX to the submit button, then implement action cable.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    chat = Chat.find(message_params[:chat_id])
+    authorize @message
+    if @message.save
+      flash[:notice] = "Your message has been sent"
+      redirect_to chat_path(chat)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :chat_id)
   end
 end
